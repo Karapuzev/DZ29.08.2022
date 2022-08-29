09f0bb1e92ff788bcdbf0cafeac8907ee450a610
Denis <kade01@yandex.ru>
2022-08-29 16:16:50 +0300

    Задача 19

---
+++ DZ_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+void CubedNumbersTable(int number)
+
+{
+    for(int i = 1; i <= number; i++)
+    {
+        int cubedNumber = i * i * i;
+        Console.WriteLine($"{i}->3 = {cubedNumber}");
+    }       
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+CubedNumbersTable(number);
